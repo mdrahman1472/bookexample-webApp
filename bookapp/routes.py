d0d030d9cc1b54ed4311acc0a4e59c6691e28e30
Md Rahman <mrahman013@citymail.cuny.edu>
2017-12-24 13:00:21 -0500

    yaml, senseandibility file, and parse_htmlbook added from professor code but template can't find senseandsensibility.html

--- bookapp/routes.py
+++ bookapp/routes.py
@@ -1,6 +1,54 @@
 from flask import render_template
+import re
+from bs4 import BeautifulSoup
 from bookapp import app
+
+
+def parse_htmlbook():
+    page = render_template('senseandsensibility.html')
+    links = get_chap_links(page)
+    sections = {}
+    for ind in range(len(links)):
+        section = {}
+        start = links[ind]
+        if ind < len(links)-1:
+            end = links[ind+1]
+            patt = ('<A NAME="' + start +
+            '"></A>(?P<sectionbody>.*)<A NAME="' +
+            end + '">' )
+            match = re.search(patt,page,re.MULTILINE|re.DOTALL)
+            if match == None:
+                raise Exception('patt: '+patt+'\n\n')
+        else:
+            patt = ('<A NAME="' + start + '"></A>(?P<sectionbody>.*)<pre>')
+            match = re.search(patt,page,re.MULTILINE|re.DOTALL)
+        if match:
+            soup = BeautifulSoup(match.group("sectionbody"), 'html.parser')
+            plist = [p.contents[0] for p in soup.find_all('p')]
+            section['title']= (soup.find('h3').contents)[0]
+            section['plist']= plist
+            sections[start] = section
+    return links, sections
+
+
+
+def get_chap_links(page):
+    soup = BeautifulSoup(page)
+    links = [str(link.get('href'))[1:]
+             for link in soup.find_all('a') if link.get('href')]
+    return links
+
 
 @app.route('/')
 def index():
+    #page = render_template('/bookexample/bookapp/static/data/senseandsensibility.html')
+    links, sections = parse_htmlbook()
+    chapter = len(links)
+    title = "Sense and Sensibility by Jane Austen"
+    section = []
+    for i in range(chapter):
+        section.append(str(sections[links[i]]['title']))
+
+    print(str(chapter))
+    print(section)
     return render_template('index.html', value='Hello World!')
